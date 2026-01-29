af0f4d27186d5f3f3e5cc1f90f50bd420e3311bc
yohannsgw <yohannsgw@gmail.com>
2026-01-29 12:33:08 +0000

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,29 @@
+print("Welcome")
+
+#First quetsion
+
+print("Is coding part of Programming ?")
+answer = input("Type here: ")
+if answer.lower() == "yes":
+  print("CORRECT !")
+else:
+  print("NOT CORRECT !")
+  
+#Second quetsion
+
+print("What is the product of 3*4 ?")
+answer = int(input("Type here : "))
+if answer == 12:
+  print("RIGHT ANSWER !")
+else:
+  print("WRONG ANSWER")
+  
+#Third quetsion
+
+answer = int(input("How many vowels are in the English Alphabet ? Type here: "))
+if answer == 5:
+  print("PASSED !")
+else: 
+  print("FAILED")
+
+ 
